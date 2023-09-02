3f57efb15ac8819c7f14c36a848dc64e60fd9990
maha-sachin <mahasachin108@gmail.com>
2023-09-02 10:34:57 -0400

    git add
    comment component added
    
     Please enter the commit message for your changes. Lines starting
    zzz
    

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const youtubeVideoApi = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" + process.env.REACT_APP_GOOGLE_API_KEY;
 
-
+export const LIVE_CHAT_COUNT = 10;
 export const YOUTUBE_SEARCH_API = "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
